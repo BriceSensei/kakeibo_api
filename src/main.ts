6a98122a8e6480545a4eadaf05e4c4a23a25a5d1
Norookezi <norookezi@pandacrp.com>
2024-01-22 14:52:14 +0100

    feat: ✨ Added main express server
    Added a base as express server to later add all routes

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const ENV: NodeJS.ProcessEnv = process.env;
+
+const app = express();
+
+/** Make routes here */
+
+const port = parseInt(ENV.PORT ?? "3000");
+
+app.listen(port, () => {
+  console.log(`Server started at ${port}`);
+});
