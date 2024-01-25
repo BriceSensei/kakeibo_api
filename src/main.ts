179ee88b48165428f2936aefada4da5b32494365
MickaelVotte <mickael.votte@hotmail.com>
2024-01-25 16:41:04 +0100

    fix port

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,6 @@ import userRoute from '@route/UserRoute';
 dotenv.config();
 const ENV: NodeJS.ProcessEnv = process.env;
 const app = express();
-const port = parseInt(ENV.PORT ?? "3000");
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
