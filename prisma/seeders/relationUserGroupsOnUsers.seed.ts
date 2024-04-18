097773ef8a60bc03c610a8e0a58bca990d71077a
Norookezi <norookezi@pandacrp.com>
2024-04-18 14:26:18 +0200

    fix: :bug: Fixed seeder relationUserGroups
    Fixed the group number that could be dupplicated

--- prisma/seeders/relationUserGroupsOnUsers.seed.ts
+++ prisma/seeders/relationUserGroupsOnUsers.seed.ts
@@ -5,14 +5,23 @@ import prisma from "../prisma";
 export async function seed() {
   await prisma.relation_UserGroupsOnUsers.deleteMany({});
 
-  const relation_UserGroupsOnUsers: Relation_UserGroupsOnUsers[] = [];
+  let groups = (await prisma.userGroups.findMany()).map(group => { return group.id; })
+
+  const relation_UserGroupsOnUsers: Relation_UserGroupsOnUsers[] = []
 
   for (let i = 0; i < 8; i++) {
-    relation_UserGroupsOnUsers.push({
-      userGroupsId: faker.number.int({min: 1, max: 8}),
+
+    const groupNumb = faker.number.int({min: 1, max: groups.length})-1
+    
+    const data = {
+      userGroupsId: groups[groupNumb],
       usersId: faker.number.int({min: 15, max: 30}),
       assignedAt: new Date(),
-    });
+    };
+
+    groups.splice(groupNumb, 1)
+
+    relation_UserGroupsOnUsers.push(data);
   }
 
   const addRelation_UserGroupsOnUsers = async () =>
