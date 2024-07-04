f08f5c0689174ed674169a770816e705e71353ef
Mickael Votte <mickael.votte@hotmail.com>
2024-07-04 17:59:53 +0200

    CRUD tips + ajout d'un dossier mailer

--- src/routes/TipsRoute.ts
+++ src/routes/TipsRoute.ts
@@ -1,20 +1,21 @@
-import express, { Request, Response, Router } from 'express';
-import { UnimplementedError } from '../exceptions/UnimplementedError';
+import express, { Request, Response, Router } from "express";
+import { UnimplementedError } from "../exceptions/UnimplementedError";
 import { TipsController } from "@controller/TipsController";
+import { authentificateToken } from "../middlewares/authentificateToken";
 
 const tipsRoute: Router = express.Router();
-const tipsController : TipsController = new TipsController;
+const tipsController: TipsController = new TipsController();
 
-tipsRoute.post('/', tipsController.createNewTip);
-tipsRoute.get('/',tipsController.getAllTips);
-tipsRoute.get('/:id', tipsController.getTipById);
+tipsRoute.post("/", authentificateToken, tipsController.createNewTip);
+tipsRoute.get("/", authentificateToken, tipsController.getAllTips);
+tipsRoute.get("/:id", authentificateToken, tipsController.getTipById);
 
 // Get all alerts from user
-tipsRoute.get('/user/:userId', (req: Request, res: Response): void => {
-    throw new UnimplementedError();
+tipsRoute.get("/user/:userId", (req: Request, res: Response): void => {
+  throw new UnimplementedError();
 });
 
-tipsRoute.patch('/:id', tipsController.updateOneTip);
-tipsRoute.delete('/:id', tipsController.deleteOneTip);
+tipsRoute.patch("/:id", tipsController.updateOneTip);
+tipsRoute.delete("/:id", tipsController.deleteOneTip);
 
-export default tipsRoute
+export default tipsRoute;
