8de1eab850157fe57303bf612bb513743df60306
Brice <briere.brice@gmail.com>
2024-10-29 22:27:25 +0100

    Refactor Jest configuration and add integration tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 export default {
   testEnvironment: "node",
-  types: ["@types/jest"],
   transform: {
     "^.+.tsx?$": ["ts-jest",{}],
   },
---
+++ src/__tests__/app.test.ts
@@ -0,0 +1,13 @@
+import request from 'supertest';
+import { app, server } from '../main'; 
+
+afterAll(() => {
+  server.close(); // Ferme le serveur après tous les tests
+});
+
+describe('GET /metrics', () => {
+  it('should return 200 OK for metrics endpoint', async () => {
+    const res = await request(app).get('/metrics');
+    expect(res.statusCode).toBe(200);
+  });
+});
